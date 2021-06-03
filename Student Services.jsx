7e38a5ea98d872fecd7908ae7c6a92f708c4fc0a
shahdZourob <79404661+shahdZourob@users.noreply.github.com>
2021-06-03 21:51:53 +0200

    Add files via upload

---
+++ Student Services.jsx
@@ -0,0 +1,293 @@
+import React from "react";
+
+function Contact() {
+  return (
+    
+
+      <div class="content">
+        <div class="col-md-2 sin-bottom" id="sidebar-wrapper">
+  <div class="sidenav" style={{width:'15%',  position:'fixed', textSize: '10px', background:'#e6f2ff'}}>
+    <h5 class=" text-center" style={{background:'#4da6ff'}}>عن الجامعة</h5>
+<div class="row">
+  <div class="col-md-10">
+    <thead>
+    <tr>
+    <th scope="col">
+      <a href="http://localhost:3000/ExamResults"><h6>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;نتائج الامتحانات</h6></a>
+      </th>
+      </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <th scope="col">
+            <a href="http://localhost:3000/ExamSchedule"><h6>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;جداول الامتحانات</h6></a>
+          </th>
+        </tr>
+      </tbody>
+
+      <tbody>
+        <tr>
+          <th scope="col">
+            <a href="http://localhost:3000/LectureSchedule"><h6>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;جداول محاضرات</h6></a>
+          </th>
+        </tr>
+      </tbody>
+
+      <tbody>
+        <tr>
+      <th scope="col">
+      <a href="http://localhost:3000/UniversityCourses"><h6>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;المقررات الجامعية</h6></a>
+      </th>
+      </tr>
+      </tbody>
+
+      <tbody>
+        <tr>
+      <th scope="col">
+      <a href="http://localhost:3000/GraduationCertificate"><h6>استخراج شهادةالتخرج</h6></a>
+      </th>
+      </tr>
+      </tbody>
+
+      <tbody>
+        <tr>
+      <th scope="col">
+      <a href="http://localhost:3000/VisionAndMission"><h6>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;لوائح الكليات</h6></a>
+      </th>
+      </tr>
+      </tbody>
+  </div>
+</div>
+  </div>
+</div>
+
+<div class="col-md-10 content-top">
+<br />
+<br />
+
+  <div class="number">
+  <h3><b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;خدمات الطلاب</b></h3>
+<hr/>
+
+    
+      
+<br />
+        <h6 className="fw-bolder">
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; تضع كلية الحاسبات و المعلومات امكانيات و موارد مالية كبيرة من أجل تقديم خدمات معلوماتية أساسية للطلاب
+        </h6>
+        <br />
+        <thead>
+    <tr>
+        <th scope="col">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:تقدم هذه الصفحة هذه النماذج من الخدمات المتمثلة في</th> 
+    </tr>
+</thead>   
+<br />
+       
+      
+<table class="table table-bordered" style={{width:'100%' , background:''}}>
+  <br />
+<div class="panel panel-info">
+  <div class="panel-heading"> 
+  <table class="table table-bordered" style={{width:'100%' , background:'#e6f2ff'}}>
+    <br />
+    <h4 class="panel-title text-info" style={{fontWeight:'bold', fontSize:'20px'}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;امتحانات وجداول</h4>
+  <br />
+  </table>
+  </div> 
+  
+<br />
+<div class="panel-body"> 
+<div class="row">
+
+  <div class="col-sm-3" style={{textAlign:'center'}}>
+    <a href="http://localhost:3000/ExamSchedule">
+      <img src="images/tables.jpg"/><br />
+        <b>جداول الامتحانات</b>
+    </a>
+  </div>
+
+  <div class="col-sm-3" style={{textAlign:'center'}}> 
+    <a href="http://localhost:3000/ExamResults">
+      <img src="images/examresults.png"/><br />
+        <b>نتائج الامتحانات</b>
+    </a>
+  </div>
+
+  <div class="col-sm-3" style={{textAlign:'center'}}> 
+    <a href="http://localhost:3000/LectureSchedule">
+      <img src="images/schedual.png"/><br />
+        <b>جداول المحاضرات</b>
+    </a>
+  </div>
+  
+
+</div>
+</div>
+     </div>
+<br />
+     </table>
+     
+    </div>
+
+<table class="table table-bordered" style={{background:''}}>
+<br />
+<div class="panel panel-success"> 
+  <div class="panel-heading"> 
+  <table class="table table-bordered" style={{width:'100%' , background:'#d6f5d6'}}>
+    <br />
+    <h4 class="panel-title text-success" style={{fontWeight:'bold', fontSize:'20px'}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;مقررات</h4> 
+  <br />
+  </table>
+  </div> 
+    
+<br />
+<div class="panel-body">
+  <div className="row">
+<div class="col-sm-3" style={{textAlign:'center'}}>
+    <a href="http://localhost:3000/VisionAndMission"> 
+      <img src="images/guids.png"/><br />
+        <b>لوائح الكليات</b>
+    </a>
+  </div>
+
+  <div class="col-sm-3" style={{textAlign:'center'}}>
+    <a href="http://localhost:3000/UniversityCourses">
+      <img src="images/courses.png" /><br /> 
+        <b>المقررات الجامعية</b>
+    </a>
+  </div>
+
+  <div class="col-sm-3" style={{textAlign:'center'}}>
+    <a href="http://localhost:3000/GraduationCertificate">
+      <img src="images/courses.png" /><br /> 
+        <b>استخراج شهادة التخرج</b>
+    </a>
+  </div>
+</div>
+  
+ </div>
+</div>
+<br />
+</table>
+
+ <table class="table table-bordered" style={{background:''}}>
+<br />
+<div class="panel panel-warning"> 
+  <div class="panel-heading"> 
+  <table class="table table-bordered" style={{width:'100%' , background:'#fff0e6'}}>
+    <br />
+   <h4 class="panel-title text-warning" style={{fontWeight:'bold', fontSize:'20px'}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;خدمات اخرى</h4> 
+  <br />
+  </table>
+  </div> 
+<br />
+<div class="panel-body"> 
+  <div class="row">
+    <div class="col-sm-3" style={{textAlign:'center'}}>
+      <a href="http://localhost:3000/Activity"> 
+        <img src="images/actvites.png"/><br />
+          <b>الانشطة الطلابية</b>
+      </a>
+    </div>
+</div>
+</div> 
+</div>
+<br />
+</table>
+
+
+<br />
+    </div>
+
+
+
+
+
+
+    <hr/>
+<footer>
+  <div className="footer-top">
+    <div className="container">
+
+      <div className="pull-right">
+        <div className="date-today"></div>
+      </div>
+      <div className="pull-left">
+        <a href="#" className="scrollup main-background" style={{display:'block'}}>
+          <span className="fa fa-arrow-up">..</span>
+          <p>الاعلى</p>
+        </a>
+      </div>
+
+    </div>    
+  </div>
+
+
+
+<hr/>
+  <div className="footer-down">
+    <div className="container">
+
+    <footer id="site-footer">
+      <footer id="footer-content">
+        <div className="row">
+          <div className="col-sm-9">
+            <nav id="footer-nav">
+              <table>
+                <thead>
+
+                  <th>
+                    <a href="#" style={{textDecoration:' none', color:'black',fontFamily:'serif'}}> Egypt - Ismailia - Kilo Ring Road 4.5 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+                  </th>
+
+                  <th>
+                    <a href="#" style={{textDecoration:' none', color:'black',fontFamily:'serif'}}> Phone: (+2064) 3223007 - 32001258 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+                  </th>
+
+                  <th>
+                    <a href="#" style={{textDecoration:' none', color:'black',fontFamily:'serif'}}> Fax:  (+202) 22621217 - (+2064) 3205208 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+                  </th>
+
+                  <th>
+                    <a href="#" style={{textDecoration:' none',color:'black',fontFamily:'serif'}}> President_office@suez.edu.eg &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+                  </th>
+
+                </thead>
+                </table>       
+            </nav>
+          </div>
+
+          <div className="col-sm-3">
+            <p className="text-left">    </p>
+          </div>
+          <br />
+          <br />
+          <div className="under" >
+<nav className="navbar navbar-expand-lg navbar-hight bg-light">
+  <div className="container-fluid">
+    <h5></h5>
+  </div>
+</nav>
+    </div>
+        </div>
+      </footer>
+    </footer>
+
+
+
+    </div>
+  </div>
+</footer>
+
+
+
+
+
+    </div>
+    
+  );
+}
+      
+
+export default Contact;
